3d250df669961395ae6b4bf6eff7a9656c538788
DarinaVT <darito.timoteeva10@gmail.com>
2025-06-29 11:07:13 +0300

    Create ElectricCarsDb

---
+++ ElectricCarsDb/ElectricCars/Models/ElectricCar.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models;
+
+public class ElectricCar : Base
+{
+    public string Vin { get; set; }
+    public int LocationId { get; set; }
+    public Location Location { get; set; }
+
+    public int MakeModelYearId { get; set; }
+    public MakeModelYear MakeModelYear { get; set; }
+
+    public string ElectricVehicleType { get; set; }
+    public string CAFVEligibility { get; set; }
+
+    public int? ElectricRange { get; set; }
+    public int? BaseMsrp { get; set; }
+    public string LegislativeDistric { get; set; }
+    public string DOLVehicleId { get; set; }
+    public string CensusTract2020 { get; set; }
+    public ICollection<ElectricVehicleUtility> VehicleUtilities { get; set; }
+
+}
