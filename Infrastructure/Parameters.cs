4a7e7e3da4ad199e1b8deafdf32efdf5165f93e2
DarinaVT <darito.timoteeva10@gmail.com>
2025-06-29 10:26:55 +0300

    Delete Infrastructure directory

--- Infrastructure/Parameters.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Infrastructure;
-
-public class Parameters
-{
-    public static string FilePath = @"D:\uni\ElectricCars\Infrastructure\Electric_Vehicle_Population_Data.csv";
-    public static string ConnectionString = @"Server=DESKTOP-ENRVS12;Database=NewElectric;Trusted_Connection=True;TrustServerCertificate=True;";
-}
